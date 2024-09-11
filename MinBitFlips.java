f8cd8c02f76fe4bfdb967a9112aeeb5fa4d692aa
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-09-11 15:35:08 +0530

    Given two integers start and goal, return the minimum number of bit flips to convert start to goal

---
+++ MinBitFlips.java
@@ -0,0 +1,20 @@
+/*
+A bit flip of a number x is choosing a bit in the binary representation of x and flipping it from either 0 to 1 or 1 to 0.
+
+For example, for x = 7, the binary representation is 111 and we may choose any bit (including any leading zeros not shown) and flip it. We can flip the first bit from the right to get 110, flip the second bit from the right to get 101, flip the fifth bit from the right (a leading zero) to get 10111, etc.
+Given two integers start and goal, return the minimum number of bit flips to convert start to goal.
+*/
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int c = start ^ goal;
+        int count = 0;
+        while(c>0){
+            if((c&1) == 1){
+                count++;
+            }
+            c = c>>1;
+        }
+
+        return count;
+    }
+}
