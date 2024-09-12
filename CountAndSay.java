3fcf0531ac80f0a78655bae67ecbc202c25fc03a
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-09-12 23:46:39 +0530

    Given a positive integer n, return the nth element of the count-and-say sequence

---
+++ CountAndSay.java
@@ -0,0 +1,29 @@
+/*
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+
+Given a positive integer n, return the nth element of the count-and-say sequence.
+*/
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+
+        String str = countAndSay(n-1);
+        String res  ="";
+        int counter =0;
+        for(int i=0; i<str.length(); i++){
+            counter++;
+            if(i==str.length()-1 || str.charAt(i) != str.charAt(i+1)){
+                res = res + counter + str.charAt(i);
+                counter = 0;
+            }
+        }
+
+        return res;
+    }
+}
