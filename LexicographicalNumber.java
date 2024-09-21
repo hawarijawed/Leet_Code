4c2e63ad681c9e075b157b94258055839a0a817a
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-09-21 21:16:36 +0530

    Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order

---
+++ LexicographicalNumber.java
@@ -0,0 +1,28 @@
+/*
+Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+*/
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> lst = new ArrayList<>();
+        for(int i=1; i<10; i++){
+            if(i>n){
+                break;
+            }
+            dfs(i, n, lst);
+        }
+        return lst;
+    }
+    public void dfs(int currNum, int target, List<Integer> lst){
+        if(currNum > target){
+            return;
+        }
+        lst.add(currNum);
+        for(int digit = 0; digit<10; digit++){
+            if(currNum*10 + digit > target){
+                break;
+            }
+            dfs(currNum*10+digit, target, lst);
+        }
+    }
+}L
