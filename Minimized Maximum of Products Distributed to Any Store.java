afce866db51dbf08e3a083af7f28d8137a05df41
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-11-14 22:03:57 +0530

    Return the possible minimum value x.

---
+++ Minimized Maximum of Products Distributed to Any Store.java
@@ -0,0 +1,38 @@
+/*
+You are given an integer n indicating there are n specialty retail stores. There are m product types of varying amounts, which are given as a 0-indexed integer array quantities, where quantities[i] represents the number of products of the ith product type.
+
+You need to distribute all products to the retail stores following these rules:
+
+A store can only be given at most one product type but can be given any amount of it.
+After distribution, each store will have been given some number of products (possibly 0). Let x represent the maximum number of products given to any store. You want x to be as small as possible, i.e., you want to minimize the maximum number of products that are given to any store.
+Return the minimum possible x.
+*/
+class Solution {
+    private boolean solve(int n, int[] quantities, int item) {
+        if (item == 0) return false;
+        int store = 0;
+        for (int product : quantities) {
+            store += (product - 1) / item + 1;
+            if (store > n) return false;
+        }
+        return true;
+    }
+
+    public int minimizedMaximum(int n, int[] quantities) {
+        int low = 1;
+        int high = Arrays.stream(quantities).max().getAsInt();
+        int ans = -1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (solve(n, quantities, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
