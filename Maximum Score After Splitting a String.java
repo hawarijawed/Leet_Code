299e402b2687adad3fc906cf9f8c5ecf67ea34a3
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-01-02 01:28:49 +0530

    return the maximum score after splitting the string into two non-empty substrings

---
+++ Maximum Score After Splitting a String.java
@@ -0,0 +1,29 @@
+/*
+Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+*/
+class Solution {
+    public int maxScore(String s) {
+        int ones = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            }
+        }
+        
+        int ans = 0;
+        int zeros = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+            
+            ans = Math.max(ans, zeros + ones);
+        }
+        
+        return ans;
+    }
+}
