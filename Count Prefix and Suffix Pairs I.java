9b71d197fb7135dc11a39aab0d2dab03b287975b
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-01-08 23:38:59 +0530

    Return an integer denoting the number of index pairs (i, j) such that i < j

---
+++ Count Prefix and Suffix Pairs I.java
@@ -0,0 +1,37 @@
+/*
+You are given a 0-indexed string array words.
+
+Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+
+isPrefixAndSuffix(str1, str2) returns true if str1 is both a 
+prefix
+ and a 
+suffix
+ of str2, and false otherwise.
+For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+
+Return an integer denoting the number of index pairs (i, j) such that i < j, and isPrefixAndSuffix(words[i], words[j]) is true.
+*/
+class Solution {
+    public boolean isPrefixAndSuffix(String st1, String st2){
+
+        return st2.startsWith(st1) && st2.endsWith(st1);
+    }
+    public int countPrefixSuffixPairs(String[] words) {
+        int count = 0;
+        int n = words.length;
+        
+        for(int i=0; i<n; i++){
+            String st1 = words[i];
+            for(int j = i+1; j<n; j++){
+        
+                String st2 = words[j];
+                if(isPrefixAndSuffix(st1, st2)){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
