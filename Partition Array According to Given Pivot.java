1828c93acd8d5d4361e070f41e5c1f6bb13ec103
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-03-03 21:23:38 +0530

    You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied

---
+++ Partition Array According to Given Pivot.java
@@ -0,0 +1,32 @@
+/*
+You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+Every element less than pivot appears before every element greater than pivot.
+Every element equal to pivot appears in between the elements less than and greater than pivot.
+The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot, then pi < pj.
+Return nums after the rearrangement.
+*/
+class Solution {
+
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] ans = new int[nums.length];
+        int lessI = 0;
+        int greaterI = nums.length - 1;
+        for (int i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+            if (nums[i] < pivot) {
+                ans[lessI] = nums[i];
+                lessI++;
+            }
+            if (nums[j] > pivot) {
+                ans[greaterI] = nums[j];
+                greaterI--;
+            }
+        }
+        while (lessI <= greaterI) {
+            ans[lessI] = pivot;
+            lessI++;
+        }
+        return ans;
+    }
+}
