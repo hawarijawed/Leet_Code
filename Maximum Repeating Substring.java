8e87defa9e11a07d533c0fcc58df3c0d5bcd9a54
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-03-22 14:18:36 +0530

    Given strings sequence and word, return the maximum k-repeating value of word in sequence

---
+++ Maximum Repeating Substring.java
@@ -0,0 +1,17 @@
+/*
+For a string sequence, a string word is k-repeating if word concatenated k times is a substring of sequence. The word's maximum k-repeating value is the highest value k where word is k-repeating in sequence. If word is not a substring of sequence, word's maximum k-repeating value is 0.
+
+Given strings sequence and word, return the maximum k-repeating value of word in sequence.
+*/
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int res = 0;
+        String temp = word;
+        while(sequence.contains(temp)){
+            res++;
+            temp = temp + word;
+        }
+
+        return res;
+    }
+}
