66fea2dfeb7345d46ab152756a7ff733a41b8a88
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-04-03 22:24:01 +0530

    Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0

---
+++ Maximum Value of an Ordered Triplet II.java
@@ -0,0 +1,24 @@
+/*
+You are given a 0-indexed integer array nums.
+
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+*/
+public class Solution {
+
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = Math.max(rightMax[n - i], nums[n - i]);
+        }
+        long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = Math.max(res, (long) (leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
+    }
+}
