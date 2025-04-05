8765a3f8e11c0c72021a341a82d27c487971b1fb
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-04-06 00:04:37 +0530

    Return the sum of XOR of all the subsets of given elements

---
+++ Sum of All Subset XOR Totals.java
@@ -0,0 +1,21 @@
+/*
+The XOR total of an array is defined as the bitwise XOR of all its elements, or 0 if the array is empty.
+
+For example, the XOR total of the array [2,5,6] is 2 XOR 5 XOR 6 = 1.
+Given an array nums, return the sum of all XOR totals for every subset of nums. 
+
+Note: Subsets with the same elements should be counted multiple times.
+
+An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b.
+*/
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int result = 0;
+        // Capture each bit that is set in any of the elements
+        for (int num : nums) {
+            result |= num;
+        }
+        // Multiply by the number of subset XOR totals that will have each bit set
+        return result << (nums.length - 1);
+    }
+}
