6251f04a1857bdfb064e04927a70db6001785a16
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-04-08 23:22:08 +0530

    Return the minimum number of operations needed to make the elements in the array distinct

---
+++ Minimum Number of Operations to Make Elements in Array Distinct.java
@@ -0,0 +1,19 @@
+/*
+You are given an integer array nums. You need to ensure that the elements in the array are distinct. To achieve this, you can perform the following operation any number of times:
+
+Remove 3 elements from the beginning of the array. If the array has fewer than 3 elements, remove all remaining elements.
+Note that an empty array is considered to have distinct elements. Return the minimum number of operations needed to make the elements in the array distinct.
+*/
+class Solution {
+
+    public int minimumOperations(int[] nums) {
+        boolean[] seen = new boolean[128];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (seen[nums[i]]) {
+                return i / 3 + 1;
+            }
+            seen[nums[i]] = true;
+        }
+        return 0;
+    }
+}
