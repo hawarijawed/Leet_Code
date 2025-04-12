8b1fc2e9c04f7e0ce44c30e09b26696444cd6bdb
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-04-13 00:38:40 +0530

    Return the count of good integers containing n digits

---
+++ Find the Count of Good Integers.java
@@ -0,0 +1,55 @@
+/*
+You are given two positive integers n and k.
+
+An integer x is called k-palindromic if:
+
+x is a palindrome.
+x is divisible by k.
+An integer is called good if its digits can be rearranged to form a k-palindromic integer. For example, for k = 2, 2020 can be rearranged to form the k-palindromic integer 2002, whereas 1010 cannot be rearranged to form a k-palindromic integer.
+
+Return the count of good integers containing n digits.
+
+Note that any integer must not have leading zeros, neither before nor after rearrangement. For example, 1010 cannot be rearranged to form 101.
+*/
+
+class Solution {
+
+    public long countGoodIntegers(int n, int k) {
+        Set<String> dict = new HashSet<>();
+        int base = (int) Math.pow(10, (n - 1) / 2);
+        int skip = n & 1;
+        /* Enumerate the number of palindrome numbers of n digits */
+        for (int i = base; i < base * 10; i++) {
+            String s = Integer.toString(i);
+            s += new StringBuilder(s).reverse().substring(skip);
+            long palindromicInteger = Long.parseLong(s);
+            /* If the current palindrome number is a k-palindromic integer */
+            if (palindromicInteger % k == 0) {
+                char[] chars = s.toCharArray();
+                Arrays.sort(chars);
+                dict.add(new String(chars));
+            }
+        }
+
+        long[] factorial = new long[n + 1];
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+        long ans = 0;
+        for (String s : dict) {
+            int[] cnt = new int[10];
+            for (char c : s.toCharArray()) {
+                cnt[c - '0']++;
+            }
+            /* Calculate permutations and combinations */
+            long tot = (n - cnt[0]) * factorial[n - 1];
+            for (int x : cnt) {
+                tot /= factorial[x];
+            }
+            ans += tot;
+        }
+
+        return ans;
+    }
+}
