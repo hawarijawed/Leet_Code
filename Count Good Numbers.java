05a7faf5cbc2740318bf476528e0f3817e25f8cf
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-04-13 23:45:11 +0530

    Return the count of good numbers

---
+++ Count Good Numbers.java
@@ -0,0 +1,31 @@
+/*
+A digit string is good if the digits (0-indexed) at even indices are even and the digits at odd indices are prime (2, 3, 5, or 7).
+
+For example, "2582" is good because the digits (2 and 8) at even positions are even and the digits (5 and 2) at odd positions are prime. However, "3245" is not good because 3 is at an even index but is not even.
+Given an integer n, return the total number of good digit strings of length n. Since the answer may be large, return it modulo 109 + 7.
+
+A digit string is a string consisting of digits 0 through 9 that may contain leading zeros
+*/
+class Solution {
+
+    long mod = 1000000007;
+
+    public int countGoodNumbers(long n) {
+        return (int) ((quickmul(5, (n + 1) / 2) * quickmul(4, n / 2)) % mod);
+    }
+
+    // use fast exponentiation to calculate x^y % mod
+    public long quickmul(int x, long y) {
+        long ret = 1;
+        long mul = x;
+        while (y > 0) {
+            if (y % 2 == 1) {
+                ret = (ret * mul) % mod;
+            }
+            mul = (mul * mul) % mod;
+            y /= 2;
+        }
+
+        return ret;
+    }
+}
