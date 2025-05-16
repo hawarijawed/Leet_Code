161262c63b555376093fb385db36e6bc2f399456
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-05-17 00:01:06 +0530

    Create Longest Unequal Adjacent Groups Subsequence II.java

---
+++ Longest Unequal Adjacent Groups Subsequence II.java
@@ -0,0 +1,63 @@
+/*
+You are given a string array words, and an array groups, both arrays having length n.
+
+The hamming distance between two strings of equal length is the number of positions at which the corresponding characters are different.
+
+You need to select the longest subsequence from an array of indices [0, 1, ..., n - 1], such that for the subsequence denoted as [i0, i1, ..., ik-1] having length k, the following holds:
+
+For adjacent indices in the subsequence, their corresponding groups are unequal, i.e., groups[ij] != groups[ij+1], for each j where 0 < j + 1 < k.
+words[ij] and words[ij+1] are equal in length, and the hamming distance between them is 1, where 0 < j + 1 < k, for all indices in the subsequence.
+Return a string array containing the words corresponding to the indices (in order) in the selected subsequence. If there are multiple answers, return any of them.
+
+Note: strings in words may be unequal in length.
+*/
+class Solution {
+
+    public List<String> getWordsInLongestSubsequence(
+        String[] words,
+        int[] groups
+    ) {
+        int n = groups.length;
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+        int maxIndex = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (
+                    check(words[i], words[j]) &&
+                    dp[j] + 1 > dp[i] &&
+                    groups[i] != groups[j]
+                ) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        for (int i = maxIndex; i >= 0; i = prev[i]) {
+            ans.add(words[i]);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    private boolean check(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (++diff > 1) {
+                    return false;
+                }
+            }
+        }
+        return diff == 1;
+    }
+}
