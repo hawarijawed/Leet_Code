fd88d0bd193084193167002f8f4fa55e7f7961c0
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-05-20 23:13:32 +0530

    Return true if it is possible to transform nums into a Zero Array after processing all the queries sequentially, otherwise return false

---
+++ Zero Array Transformation I.java
@@ -0,0 +1,35 @@
+/*
+You are given an integer array nums of length n and a 2D array queries, where queries[i] = [li, ri].
+
+For each queries[i]:
+
+Select a subset of indices within the range [li, ri] in nums.
+Decrement the values at the selected indices by 1.
+A Zero Array is an array where all elements are equal to 0.
+
+Return true if it is possible to transform nums into a Zero Array after processing all the queries sequentially, otherwise return false.
+*/
+class Solution {
+
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int[] deltaArray = new int[nums.length + 1];
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }
+        int[] operationCounts = new int[deltaArray.length];
+        int currentOperations = 0;
+        for (int i = 0; i < deltaArray.length; i++) {
+            currentOperations += deltaArray[i];
+            operationCounts[i] = currentOperations;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
