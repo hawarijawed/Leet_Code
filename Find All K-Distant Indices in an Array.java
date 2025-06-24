e5ab3c96111e941ffa0bcbe4a7e573cb9bfc95e0
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-06-24 22:52:06 +0530

    Return a list of all k-distant indices sorted in increasing order.

---
+++ Find All K-Distant Indices in an Array.java
@@ -0,0 +1,23 @@
+/*
+You are given a 0-indexed integer array nums and two integers key and k. A k-distant index is an index i of nums for which there exists at least one index j such that |i - j| <= k and nums[j] == key.
+
+Return a list of all k-distant indices sorted in increasing order.
+*/
+class Solution {
+
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> res = new ArrayList<>();
+        int r = 0; // unjudged minimum index
+        int n = nums.length;
+        for (int j = 0; j < n; ++j) {
+            if (nums[j] == key) {
+                int l = Math.max(r, j - k);
+                r = Math.min(n - 1, j + k) + 1;
+                for (int i = l; i < r; ++i) {
+                    res.add(i);
+                }
+            }
+        }
+        return res;
+    }
+}
