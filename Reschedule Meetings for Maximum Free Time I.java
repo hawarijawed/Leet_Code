40af92591c25a6f5d64653f114a92cf557def5d0
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-07-09 15:01:56 +0530

    Return the maximum amount of free time possible after rearranging the meetings

---
+++ Reschedule Meetings for Maximum Free Time I.java
@@ -0,0 +1,34 @@
+/*
+You are also given two integer arrays startTime and endTime, each of length n. These represent the start and end time of n non-overlapping meetings, where the ith meeting occurs during the time [startTime[i], endTime[i]].
+
+You can reschedule at most k meetings by moving their start time while maintaining the same duration, to maximize the longest continuous period of free time during the event.
+
+The relative order of all the meetings should stay the same and they should remain non-overlapping.
+
+Return the maximum amount of free time possible after rearranging the meetings.
+
+Note that the meetings can not be rescheduled to a time outside the event.
+*/
+public class Solution {
+
+    public int maxFreeTime(
+        int eventTime,
+        int k,
+        int[] startTime,
+        int[] endTime
+    ) {
+        int n = startTime.length;
+        int res = 0;
+        int t = 0;
+        for (int i = 0; i < n; i++) {
+            t += endTime[i] - startTime[i];
+            int left = i <= k - 1 ? 0 : endTime[i - k];
+            int right = i == n - 1 ? eventTime : startTime[i + 1];
+            res = Math.max(res, right - left - t);
+            if (i >= k - 1) {
+                t -= endTime[i - k + 1] - startTime[i - k + 1];
+            }
+        }
+        return res;
+    }
+}
