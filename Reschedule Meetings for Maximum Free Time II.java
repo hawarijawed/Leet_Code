6db29696892ce2748d93fe8e6aee30df25fb67ec
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-07-10 17:42:27 +0530

    Return the maximum amount of free time possible after rearranging the meetings

---
+++ Reschedule Meetings for Maximum Free Time II.java
@@ -0,0 +1,47 @@
+/*
+You are given an integer eventTime denoting the duration of an event. You are also given two integer arrays startTime and endTime, each of length n.
+
+These represent the start and end times of n non-overlapping meetings that occur during the event between time t = 0 and time t = eventTime, where the ith meeting occurs during the time [startTime[i], endTime[i]].
+
+You can reschedule at most one meeting by moving its start time while maintaining the same duration, such that the meetings remain non-overlapping, to maximize the longest continuous period of free time during the event.
+
+Return the maximum amount of free time possible after rearranging the meetings.
+
+Note that the meetings can not be rescheduled to a time outside the event and they should remain non-overlapping.
+
+Note: In this version, it is valid for the relative ordering of the meetings to change after rescheduling one meeting.
+*/
+
+class Solution {
+
+    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        boolean[] q = new boolean[n];
+        for (int i = 0, t1 = 0, t2 = 0; i < n; i++) {
+            if (endTime[i] - startTime[i] <= t1) {
+                q[i] = true;
+            }
+            t1 = Math.max(t1, startTime[i] - (i == 0 ? 0 : endTime[i - 1]));
+
+            if (endTime[n - i - 1] - startTime[n - i - 1] <= t2) {
+                q[n - i - 1] = true;
+            }
+            t2 = Math.max(
+                t2,
+                (i == 0 ? eventTime : startTime[n - i]) - endTime[n - i - 1]
+            );
+        }
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i == 0 ? 0 : endTime[i - 1];
+            int right = i == n - 1 ? eventTime : startTime[i + 1];
+            if (q[i]) {
+                res = Math.max(res, right - left);
+            } else {
+                res = Math.max(res, right - left - (endTime[i] - startTime[i]));
+            }
+        }
+        return res;
+    }
+}
