2d3e8de782a4a9d01d147d1ae323a4acf0669e7d
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-07-15 22:20:24 +0530

    Check if the given word is valid or not using the given conditions

---
+++ Valid Word.java
@@ -0,0 +1,39 @@
+/*
+A word is considered valid if:
+
+It contains a minimum of 3 characters.
+It contains only digits (0-9), and English letters (uppercase and lowercase).
+It includes at least one vowel.
+It includes at least one consonant.
+You are given a string word.
+
+Return true if word is valid, otherwise, return false.
+*/
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        HashSet<Character> vowels = new HashSet<>(Arrays.asList(
+            'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+        ));
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) {
+                return false; // contains invalid character
+            }
+
+            if (Character.isLetter(ch)) {
+                if (vowels.contains(ch)) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
