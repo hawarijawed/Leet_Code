c88639bfca81e05d26e7477c41640a9c4a2b7d43
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-07-17 20:57:53 +0530

    Find the Maximum Length of Valid Subsequence II

---
+++ Find the Maximum Length of Valid Subsequence II.java
@@ -0,0 +1,22 @@
+/*
+You are given an integer array nums and a positive integer k.
+A subsequence sub of nums with length x is called valid if it satisfies:
+
+(sub[0] + sub[1]) % k == (sub[1] + sub[2]) % k == ... == (sub[x - 2] + sub[x - 1]) % k.
+Return the length of the longest valid subsequence of nums.
+*/
+class Solution {
+
+    public int maximumLength(int[] nums, int k) {
+        int[][] dp = new int[k][k];
+        int res = 0;
+        for (int num : nums) {
+            num %= k;
+            for (int prev = 0; prev < k; prev++) {
+                dp[prev][num] = dp[num][prev] + 1;
+                res = Math.max(res, dp[prev][num]);
+            }
+        }
+        return res;
+    }
+}
