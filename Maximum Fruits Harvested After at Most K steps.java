1309fecbc0c2eb38a58f3d807d8aea0e52156a83
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-08-03 22:25:20 +0545

    Return the maximum total number of fruits you can harvest

---
+++ Maximum Fruits Harvested After at Most K steps.java
@@ -0,0 +1,67 @@
+/*
+Fruits are available at some positions on an infinite x-axis. You are given a 2D integer array fruits where fruits[i] = [positioni, amounti] depicts amounti fruits at the position positioni. fruits is already sorted by positioni in ascending order, and each positioni is unique.
+
+You are also given an integer startPos and an integer k. Initially, you are at the position startPos. From any position, you can either walk to the left or right. It takes one step to move one unit on the x-axis, and you can walk at most k steps in total. For every position you reach, you harvest all the fruits at that position, and the fruits will disappear from that position.
+
+Return the maximum total number of fruits you can harvest.
+*/
+
+class Solution {
+
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int n = fruits.length;
+        int[] sum = new int[n + 1];
+        int[] indices = new int[n];
+        sum[0] = 0;
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + fruits[i][1];
+            indices[i] = fruits[i][0];
+        }
+        int ans = 0;
+        for (int x = 0; x <= k / 2; x++) {
+            /* move left x steps, then move right k - x steps. */
+            int y = k - 2 * x;
+            int left = startPos - x;
+            int right = startPos + y;
+            int start = lowerBound(indices, 0, n - 1, left);
+            int end = upperBound(indices, 0, n - 1, right);
+            ans = Math.max(ans, sum[end] - sum[start]);
+            /* move right x steps, then move left k - x steps. */
+            y = k - 2 * x;
+            left = startPos - y;
+            right = startPos + x;
+            start = lowerBound(indices, 0, n - 1, left);
+            end = upperBound(indices, 0, n - 1, right);
+            ans = Math.max(ans, sum[end] - sum[start]);
+        }
+        return ans;
+    }
+
+    public int lowerBound(int[] arr, int left, int right, int val) {
+        int res = right + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= val) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public int upperBound(int[] arr, int left, int right, int val) {
+        int res = right + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > val) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
