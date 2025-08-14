5b21d580ec3be265f032b550cf1358e4ddb1896b
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-08-14 21:28:43 +0545

    Return the maximum good integer as a string or an empty string "" if no such integer exists

---
+++ Largest 3-Same-Digit Number in String.java
@@ -0,0 +1,27 @@
+/*
+You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+It is a substring of num with length 3.
+It consists of only one unique digit.
+Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+Note:
+
+A substring is a contiguous sequence of characters within a string.
+There may be leading zeroes in num or a good integer.
+*/
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        String [] str1 = {"999", "888", "777", "666", "555", "444", "333", "444", "333", "222", "111", "000"};
+        String res = "";
+        for(int i=0; i<str1.length; i++){
+            if(num.contains(str1[i])){
+                res = str1[i];
+                break;
+            }
+        }
+
+        return res;
+    }
+}
