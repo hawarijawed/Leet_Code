83317b17fe75d0b7f5b9ee843546e722e8948d28
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-09-10 19:46:35 +0545

    Create Minimum Number of People to Teach.java

---
+++ Minimum Number of People to Teach.java
@@ -0,0 +1,44 @@
+/*
+On a social network consisting of m users and some friendships between users, two users can communicate with each other if they know a common language.
+
+You are given an integer n, an array languages, and an array friendships where:
+
+There are n languages numbered 1 through n,
+languages[i] is the set of languages the i​​​​​​th​​​​ user knows, and
+friendships[i] = [u​​​​​​i​​​, v​​​​​​i] denotes a friendship between the users u​​​​​​​​​​​i​​​​​ and vi.
+You can choose one language and teach it to some users so that all friends can communicate with each other. Return the minimum number of users you need to teach.
+
+Note that friendships are not transitive, meaning if x is a friend of y and y is a friend of z, this doesn't guarantee that x is a friend of z.
+*/
+class Solution {
+
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        Set<Integer> cncon = new HashSet<>();
+        for (int[] friendship : friendships) {
+            Map<Integer, Integer> mp = new HashMap<>();
+            boolean conm = false;
+            for (int lan : languages[friendship[0] - 1]) {
+                mp.put(lan, 1);
+            }
+            for (int lan : languages[friendship[1] - 1]) {
+                if (mp.containsKey(lan)) {
+                    conm = true;
+                    break;
+                }
+            }
+            if (!conm) {
+                cncon.add(friendship[0] - 1);
+                cncon.add(friendship[1] - 1);
+            }
+        }
+        int max_cnt = 0;
+        int[] cnt = new int[n + 1];
+        for (int friendship : cncon) {
+            for (int lan : languages[friendship]) {
+                cnt[lan]++;
+                max_cnt = Math.max(max_cnt, cnt[lan]);
+            }
+        }
+        return cncon.size() - max_cnt;
+    }
+}
