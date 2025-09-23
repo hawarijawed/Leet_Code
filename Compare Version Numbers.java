b53aaa4612f3bd162244cd9f9d6502a89c0be430
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-09-23 22:58:04 +0545

    Create Compare Version Numbers.java

---
+++ Compare Version Numbers.java
@@ -0,0 +1,26 @@
+/*
+Given two version strings, version1 and version2, compare them. A version string consists of revisions separated by dots '.'. The value of the revision is its integer conversion ignoring leading zeros.
+
+To compare version strings, compare their revision values in left-to-right order. If one of the version strings has fewer revisions, treat the missing revision values as 0.
+
+Return the following:
+
+If version1 < version2, return -1.
+If version1 > version2, return 1.
+Otherwise, return 0
+*/
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int len = Math.max(v1.length, v2.length);
+        for (int i = 0; i < len; i++) {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
+        }
+        return 0;
+    }
+}
