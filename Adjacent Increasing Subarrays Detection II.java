98400a44fe530a3e0c3f605be96f6c42a32dd1e9
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-10-15 12:15:32 +0545

    Return the maximum possible value of k.

---
+++ Adjacent Increasing Subarrays Detection II.java
@@ -0,0 +1,31 @@
+/*
+Given an array nums of n integers, your task is to find the maximum value of k for which there exist two adjacent subarrays of length k each, such that both subarrays are strictly increasing. Specifically, check if there are two subarrays of length k starting at indices a and b (a < b), where:
+
+Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+The subarrays must be adjacent, meaning b = a + k.
+Return the maximum possible value of k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+*/
+class Solution {
+
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        int cnt = 1;
+        int precnt = 0;
+        int ans = 0;
+
+        for (int i = 1; i < n; ++i) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                ++cnt;
+            } else {
+                precnt = cnt;
+                cnt = 1;
+            }
+            ans = Math.max(ans, Math.min(precnt, cnt));
+            ans = Math.max(ans, cnt / 2);
+        }
+
+        return ans;
+    }
+}
