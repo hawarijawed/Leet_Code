9e9fe2c601866347c4b5ca3e99468ee1fee7ab6d
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-10-18 22:15:04 +0545

    Return the maximum possible number of distinct elements in nums after performing the operations.

---
+++ Maximum Number of Distinct Elements After Operations.java
@@ -0,0 +1,24 @@
+/*
+You are given an integer array nums and an integer k.
+
+You are allowed to perform the following operation on each element of the array at most once:
+
+Add an integer in the range [-k, k] to the element.
+Return the maximum possible number of distinct elements in nums after performing the operations.
+*/
+class Solution {
+
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int cnt = 0;
+        int prev = Integer.MIN_VALUE;
+        for (int num : nums) {
+            int curr = Math.min(Math.max(num - k, prev + 1), num + k);
+            if (curr > prev) {
+                cnt++;
+                prev = curr;
+            }
+        }
+        return cnt;
+    }
+}
