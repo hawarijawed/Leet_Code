195ae99186f72a58e159cd8e4a24e407543b56c4
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-10-24 18:26:27 +0545

    Given an integer n, return the smallest numerically balanced number strictly greater than n.

---
+++ Next Greater Numerically Balanced Number.java
@@ -0,0 +1,30 @@
+/*
+An integer x is numerically balanced if for every digit d in the number x, there are exactly d occurrences of that digit in x.
+
+Given an integer n, return the smallest numerically balanced number strictly greater than n.
+*/
+class Solution {
+
+    public int nextBeautifulNumber(int n) {
+        for (int i = n + 1; i <= 1224444; ++i) {
+            if (isBalance(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isBalance(int x) {
+        int[] count = new int[10];
+        while (x > 0) {
+            count[x % 10]++;
+            x /= 10;
+        }
+        for (int d = 0; d < 10; ++d) {
+            if (count[d] > 0 && count[d] != d) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
