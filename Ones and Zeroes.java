9c31ed059bcde90b0cec7f1d0f94ea2b18d6f786
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-11-11 22:01:31 +0545

    Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.

---
+++ Ones and Zeroes.java
@@ -0,0 +1,29 @@
+/*
+You are given an array of binary strings strs and two integers m and n.
+
+Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+
+A set x is a subset of a set y if all elements of x are also elements of y.
+*/
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (String s : strs) {
+            int zeros = 0, ones = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '0') zeros++;
+                else ones++;
+            }
+
+            // Traverse backwards to avoid using the same string multiple times
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], 1 + dp[i - zeros][j - ones]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
