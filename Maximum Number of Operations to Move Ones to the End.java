7726eac86088fece221195bf00a8726c2525683b
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-11-13 22:01:22 +0545

    Return the maximum number of operations that you can perform.

---
+++ Maximum Number of Operations to Move Ones to the End.java
@@ -0,0 +1,29 @@
+/*
+You are given a binary string s.
+
+You can perform the following operation on the string any number of times:
+
+Choose any index i from the string where i + 1 < s.length such that s[i] == '1' and s[i + 1] == '0'.
+Move the character s[i] to the right until it reaches the end of the string or another '1'. For example, for s = "010010", if we choose i = 1, the resulting string will be s = "000110".
+Return the maximum number of operations that you can perform.
+*/
+public class Solution {
+
+    public int maxOperations(String s) {
+        int countOne = 0;
+        int ans = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '0') {
+                while (i + 1 < s.length() && s.charAt(i + 1) == '0') {
+                    i++;
+                }
+                ans += countOne;
+            } else {
+                countOne++;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
