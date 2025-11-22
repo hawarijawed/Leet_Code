8242b5e087069379db44902af87303c6a3a7be91
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-11-22 22:10:32 +0545

    Return the minimum number of operations to make all elements of nums divisible by 3.

---
+++ Find Minimum Operations to Make All Elements Divisible by Three.java
@@ -0,0 +1,17 @@
+/*
+You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+
+Return the minimum number of operations to make all elements of nums divisible by 3.
+*/
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int res = 0;
+        for(int val: nums){
+            if(val%3 != 0){
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
