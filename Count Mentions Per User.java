6605d8885a8c0cfb4a76c6c6abbed746e1ec303a
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-12-12 22:32:31 +0545

    Return an array mentions where mentions[i] represents the number of mentions the user with id i has across all MESSAGE events.

---
+++ Count Mentions Per User.java
@@ -0,0 +1,68 @@
+/*
+You are given an integer numberOfUsers representing the total number of users and an array events of size n x 3.
+
+Each events[i] can be either of the following two types:
+
+Message Event: ["MESSAGE", "timestampi", "mentions_stringi"]
+This event indicates that a set of users was mentioned in a message at timestampi.
+The mentions_stringi string can contain one of the following tokens:
+id<number>: where <number> is an integer in range [0,numberOfUsers - 1]. There can be multiple ids separated by a single whitespace and may contain duplicates. This can mention even the offline users.
+ALL: mentions all users.
+HERE: mentions all online users.
+Offline Event: ["OFFLINE", "timestampi", "idi"]
+This event indicates that the user idi had become offline at timestampi for 60 time units. The user will automatically be online again at time timestampi + 60.
+Return an array mentions where mentions[i] represents the number of mentions the user with id i has across all MESSAGE events.
+
+All users are initially online, and if a user goes offline or comes back online, their status change is processed before handling any message event that occurs at the same timestamp.
+
+Note that a user can be mentioned multiple times in a single message event, and each mention should be counted separately.
+*/
+class Solution {
+
+    public int[] countMentions(int numberOfUsers, List<List<String>> events) {
+        events.sort((a, b) -> {
+            int timeA = Integer.parseInt(a.get(1));
+            int timeB = Integer.parseInt(b.get(1));
+            if (timeA != timeB) {
+                return Integer.compare(timeA, timeB);
+            }
+            boolean aIsMessage = a.get(0).equals("MESSAGE");
+            boolean bIsMessage = b.get(0).equals("MESSAGE");
+            return Boolean.compare(aIsMessage, bIsMessage);
+        });
+
+        int[] count = new int[numberOfUsers];
+        int[] nextOnlineTime = new int[numberOfUsers];
+
+        for (List<String> event : events) {
+            int curTime = Integer.parseInt(event.get(1));
+            String type = event.get(0);
+
+            if (type.equals("MESSAGE")) {
+                String target = event.get(2);
+                if (target.equals("ALL")) {
+                    for (int i = 0; i < numberOfUsers; i++) {
+                        count[i]++;
+                    }
+                } else if (target.equals("HERE")) {
+                    for (int i = 0; i < numberOfUsers; i++) {
+                        if (nextOnlineTime[i] <= curTime) {
+                            count[i]++;
+                        }
+                    }
+                } else {
+                    String[] users = target.split(" ");
+                    for (String user : users) {
+                        int idx = Integer.parseInt(user.substring(2));
+                        count[idx]++;
+                    }
+                }
+            } else {
+                int idx = Integer.parseInt(event.get(2));
+                nextOnlineTime[idx] = curTime + 60;
+            }
+        }
+
+        return count;
+    }
+}
