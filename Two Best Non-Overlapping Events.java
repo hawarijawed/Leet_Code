1ec8bb4e5d96df4381d69bac7dc51ef58b73c3c6
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-12-23 23:30:53 +0545

    Create Two Best Non-Overlapping Events.java

---
+++ Two Best Non-Overlapping Events.java
@@ -0,0 +1,38 @@
+/*
+You are given a 0-indexed 2D integer array of events where events[i] = [startTimei, endTimei, valuei]. The ith event starts at startTimei and ends at endTimei, and if you attend this event, you will receive a value of valuei. You can choose at most two non-overlapping events to attend such that the sum of their values is maximized.
+
+Return this maximum sum.
+
+Note that the start time and end time is inclusive: that is, you cannot attend two events where one of them starts and the other ends at the same time. More specifically, if you attend an event with end time t, the next event must start at or after t + 1.
+*/
+class Solution {
+
+    public int maxTwoEvents(int[][] events) {
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        int[][] dp = new int[events.length][3];
+        for (int[] d : dp) Arrays.fill(d, -1);
+        return findEvents(events, 0, 0, dp);
+    }
+
+    // Recursive function to find the greatest sum for the pairs.
+    int findEvents(int[][] events, int idx, int cnt, int[][] dp) {
+        if (cnt == 2 || idx >= events.length) return 0;
+        if (dp[idx][cnt] == -1) {
+            int end = events[idx][1];
+            int lo = idx + 1, hi = events.length - 1;
+            while (lo < hi) {
+                int mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end) hi = mid;
+                else lo = mid + 1;
+            }
+            int include =
+                events[idx][2] +
+                (lo < events.length && events[lo][0] > end
+                        ? findEvents(events, lo, cnt + 1, dp)
+                        : 0);
+            int exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = Math.max(include, exclude);
+        }
+        return dp[idx][cnt];
+    }
+}
